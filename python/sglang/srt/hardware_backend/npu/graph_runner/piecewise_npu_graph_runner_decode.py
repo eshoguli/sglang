9eff0bb7582530ad1590745d1ae3b169439b2690
Edward Shogulin <edward.shogulin@gmail.com>
2025-12-16 09:32:11 +0300

    PiecewiseGraph runner quick fix

--- python/sglang/srt/hardware_backend/npu/graph_runner/piecewise_npu_graph_runner_decode.py
+++ python/sglang/srt/hardware_backend/npu/graph_runner/piecewise_npu_graph_runner_decode.py
@@ -40,9 +40,7 @@ from sglang.srt.hardware_backend.npu.graph_runner.compilation.piecewise_npu_grap
     PiecewiseNpuGraphCompiler,
 )
 from sglang.srt.layers.logits_processor import LogitsProcessorOutput
-from sglang.srt.model_executor.cuda_graph_runner import (
-    CudaGraphRunner,
-)
+from sglang.srt.model_executor.cuda_graph_runner import CudaGraphRunner
 from sglang.srt.model_executor.forward_batch_info import (
     CaptureHiddenMode,
     ForwardBatch,
@@ -109,7 +107,6 @@ class PiecewiseNPUGraphRunnerDecode(CudaGraphRunner):
         self.graphs = {}
         self.output_buffers = {}
         self.enable_torch_compile = model_runner.server_args.enable_torch_compile
-        self.enable_dp_attention = model_runner.server_args.enable_dp_attention
 
         # Graph inputs
         with torch.device(self.model_runner.device):
@@ -184,7 +181,6 @@ class PiecewiseNPUGraphRunnerDecode(CudaGraphRunner):
         num_token_non_padded = None
 
         assert self.pp_size <= 1
-        assert self.enable_dp_attention == False
         global_num_tokens = None
         gathered_buffer = None
 
@@ -235,10 +231,7 @@ class PiecewiseNPUGraphRunnerDecode(CudaGraphRunner):
             forward_batch.req_pool_indices[i] = 1
         forward_batch.seq_lens_sum = sum(forward_batch.seq_lens)
 
-        if self.enable_dp_attention:  # or self.enable_sp_layernorm:
-            assert False
         assert self.pp_size <= 1
-        assert self.enable_dp_attention == False
         assert enable_num_token_non_padded(self.model_runner.server_args) == False
         assert self.enable_two_batch_overlap == False
 
@@ -379,9 +372,6 @@ class PiecewiseNPUGraphRunnerDecode(CudaGraphRunner):
             compiled_graph.forward_batch.mrope_positions[:, :raw_num_token].copy_(
                 forward_batch.mrope_positions
             )
-
-        if self.enable_dp_attention:
-            assert False
 
         if enable_num_token_non_padded(self.model_runner.server_args):
             assert False
